5754f21118d7ac8d3cc3653807533a07dc093954
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-10 09:22:51 +0100

    enhance ex2 logic

--- Ex2/contains.py
+++ Ex2/contains.py
@@ -4,27 +4,43 @@ def includeEnsemble(text, ensemble):
     return False
 
 def union(s1, s2):
-    uni = []
-    seen = []
-    for text in (s1 | s2):
-        if text in seen:
-            continue
-        seen.append(text)
-        uni.append(text)
-    return uni
+    # uni = []
+    # seen = []
+    # for text in (s1 | s2):
+    #     if text in seen:
+    #         continue
+    #     seen.append(text)
+    #     uni.append(text)
+    # return uni
+    return list(difference(list(s1), list(s2))) + list(s2)
 
-
-def compare():
+def intersection(s1, s2):
     intersection = []
-    diff = []
     for text in s1:
         if includeEnsemble(text, s2):
             intersection.append(text)
-        else:
+    return intersection
+
+def difference(s1, s2):
+    diff = []
+    for text in s1:
+        if not includeEnsemble(text, s2):
             diff.append(text)
+    return diff
+
+def compare():
+    # intersection = []
+    # diff = []
+    # for text in s1:
+    #     if includeEnsemble(text, s2):
+    #         intersection.append(text)
+    #     else:
+    #         diff.append(text)
     # uni = list(set(s1).union(s2))
+    inter = intersection(s1, s2)
+    diff = difference(s1, s2)
     uni = union(s1, s2)
-    print(intersection)
+    print(inter)
     print(diff)
     print(uni)
 
