fab69daf767183bb0a0d3e9806a136f3d3492dd1
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-10 08:44:20 +0100

    TP1: ex1

---
+++ sem.py
@@ -0,0 +1,25 @@
+
+def load():
+    txt = open("data.csv").read()
+    return txt
+
+def save(txt):
+    newfile = "dataSave.csv"
+    with open(newfile, 'w') as f:
+        for line in txt:
+            f.write(line)
+
+def csv_join(csv):
+    joined_txt = "\n".join(map(lambda x: ",".join(x), csv))
+    return joined_txt
+
+def csv_split(txt):
+    return map(lambda x: x.split(","), txt.split("\n"))
+
+txt = load()
+csv = csv_split(txt)
+save(txt)
+csv_joined = csv_join(csv)
+
+print(txt)
+print(csv_joined)
